cb32273ab8f1aa74c74b12b3451d45ede53590b4
2dam <2dam@HZ375757.TartangaLH.eus>
2021-10-22 14:14:39 +0200

    Client first commit

---
+++ src/application/Application.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
