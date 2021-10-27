443fcf380874c1e05c212d11c09bfc7ba2c282f0
adriliko28@gmail.com <2dam@HZ375759.TartangaLH.eus>
2021-10-27 14:12:24 +0200

    SignUp controller exceptions treatment (on progress).

--- src/view/controllers/SignUpController.java
+++ src/view/controllers/SignUpController.java
@@ -5,8 +5,14 @@
  */
 package view.controllers;
 
+import exceptions.FieldsEmptyException;
+import exceptions.FullNameGapException;
+import exceptions.MaxCharactersException;
+import exceptions.PassMinCharacterException;
+import exceptions.PassNotEqualException;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
@@ -16,7 +22,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -34,6 +42,8 @@ import javafx.stage.WindowEvent;
  * @author 2dam
  */
 public class SignUpController extends Application {
+
+    private static final Logger logger = Logger.getLogger(SignUpController.class.getName());
 
     private Stage stage;
     @FXML
@@ -109,31 +119,78 @@ public class SignUpController extends Application {
         stage.setResizable(true);
         //Event when the window is started
         //stage.setOnShowing(this::handleWindowShowing);
-        //Event when the button is clicked
-        btnSignUp.setOnAction(this::handleButtonPressed);
-        btnCancel.setOnAction(this::handleButtonPressed);
+        //Event when the button is clicked      
         stage.show();
 
     }
 
     //ALL THE HANDLERS
     @FXML
-    private void handleButtonPressed(javafx.event.ActionEvent event) {
-        // Alert alert = new Alert(Alert.AlertType.ERROR);
-        //alert.showAndWait();
-        if (event.getSource().equals(btnSignUp)) {
-            try {
-                if (tfUser.getText().trim().isEmpty()
-                        || tfFullName.getText().trim().isEmpty()
-                        || passField.getText().trim().isEmpty())
-                        {
-                    throw new Exception();
-                }
-                //if()
-            } catch (Exception e) {
+    private void signUp(javafx.event.ActionEvent event) {
 
+        try {
+            if (checkFields()) {
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Confirmation Dialog");
+                alert.setHeaderText("Look, a Confirmation Dialog");
+                alert.setContentText("Are you ok with this?");
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK) {
+                    Alert alert1 = new Alert(AlertType.INFORMATION);
+                    alert1.setTitle("Information Dialog");
+                    alert1.setHeaderText(null);
+                    alert1.setContentText("I have a great message for you!");
+
+                    alert1.showAndWait();
+                } else {
+                    // ... user chose CANCEL or closed the dialog
+                }
             }
-        } else if (event.getSource().equals(btnCancel)) {
+
+        } catch (Exception ex) {
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText(null);
+            alert.setContentText(ex.getMessage());
+            alert.showAndWait();
+            logger.warning(ex.getClass().getSimpleName() + " exception thrown at signUp method");
+        }
+
+    }
+
+    private boolean checkFields() throws FieldsEmptyException, MaxCharactersException, PassNotEqualException, PassMinCharacterException, FullNameGapException {
+
+        if (tfUser.getText().trim().isEmpty()
+                || tfFullName.getText().trim().isEmpty()
+                || passField.getText().trim().isEmpty()
+                || rptPassword.getText().trim().isEmpty()
+                || tfEmail.getText().trim().isEmpty()) {
+            throw new FieldsEmptyException();
+        }
+
+        if (tfUser.getText().trim().length() > 255
+                || tfFullName.getText().trim().length() > 255
+                || passField.getText().trim().length() > 255
+                || rptPassword.getText().trim().length() > 255
+                || tfEmail.getText().trim().length() > 255) {
+            throw new MaxCharactersException();
+        }
+
+        if (!passField.getText().equals(rptPassword.getText())) {
+            throw new PassNotEqualException();
+        }
+
+        if (passField.getText().trim().length() < 6
+                || rptPassword.getText().trim().length() < 6) {
+            throw new PassMinCharacterException();
+        }
+        if(!tfFullName.getText().trim().contains(" ")){
+        throw new FullNameGapException();}
+        return true;
+    }
+
+    /*if (event.getSource().equals(btnCancel)) {
             Platform.exit();
             try {
                 stop();
@@ -141,9 +198,7 @@ public class SignUpController extends Application {
                 Logger.getLogger(SignUpController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-
-    }
-
+     */
     /**
      * @param args the command line arguments
      */
@@ -166,6 +221,19 @@ public class SignUpController extends Application {
      */
     public void setStage(Stage stage) {
         this.stage = stage;
+    }
+
+    @FXML
+    private void cancel(javafx.event.ActionEvent event) {
+    
+            Platform.exit();
+            try {
+                stop();
+            } catch (Exception ex) {
+                Logger.getLogger(SignUpController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        
     }
 
 }
